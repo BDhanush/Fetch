6c38c3ea67a59cc6c1f11c59a769c94261dab625
Dhanush <dhanushbommavaram@gmail.com>
2024-11-21 15:54:26 -0600

    status bar color

--- app/src/main/java/com/dhanush/fetch/MainActivity.kt
+++ app/src/main/java/com/dhanush/fetch/MainActivity.kt
@@ -19,7 +19,6 @@ private const val dataURL="https://fetch-hiring.s3.amazonaws.com/"              
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
